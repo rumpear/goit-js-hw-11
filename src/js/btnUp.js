2733da756271b05981fee3ae831f25120c9bcc9e
Roman <77742141+rumpear@users.noreply.github.com>
2022-02-14 19:19:33 +0200

    Create btnUp.js

---
+++ src/js/btnUp.js
@@ -0,0 +1,19 @@
+import throttle from 'lodash.throttle';
+
+const btnUpRef = document.querySelector('.btn-up');
+
+btnUpRef.addEventListener('click', () => {
+  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+});
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    btnUpRef.classList.remove('is-hidden');
+  } else {
+    btnUpRef.classList.add('is-hidden');
+  }
+  // console.log(1);
+}
+
+window.addEventListener('scroll', throttle(scrollFunction, 600));
